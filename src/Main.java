463523999d79bebed338465eb9ba36a3c6c4ff32
olesy <olesyagalieva97@gmail.com>
2022-12-04 10:42:58 +0500

    hi1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static int[] arr = generatorRandomArray();
+    public static int [] generatorRandomArray(){
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[10];
+        for (int i = 0;i < arr.length; i ++){
+            arr[i]= random.nextInt(100_000)+100_000;
+        }
+        return arr;
+    }
+    public static void main (String[] args){
+        int [] arr= generatorRandomArray();
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                arr[i]+=1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
+
+
+
